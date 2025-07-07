2f11c5d4eba405a3a00e51869dda93835382e81e
carloalbertogiordano <carloalbertogiordano@hotmail.com>
2025-07-07 18:34:22 +0200

    Add cost functions

--- utils/graphUtils.py
+++ utils/graphUtils.py
@@ -1,5 +1,6 @@
 import igraph as ig
 import os
+import random
 
 def get_subgraph(graph: ig.Graph, number: int):
     return graph.induced_subgraph(list(range(number)))
@@ -22,3 +23,12 @@ def save_plot(graph: ig.Graph, file_path: str, vertex_number=0) :
         vertex_label=None,
         bbox=(1000, 1000),
     )
+
+def cost1(node: ig.Vertex, rangeLow: int, rangeMax: int) :
+    return random.random(rangeLow, rangeMax)
+
+def cost2(node: ig.Vertex) :
+    return node.degree() / 2
+
+def cost3(node: ig.Vertex) :
+    return cost1() # TODO: Creare una funzione ad hoc
