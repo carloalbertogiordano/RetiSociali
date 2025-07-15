f0e67b3ef930b581dcfeb90e1a7b94921e0a8f41
carloalbertogiordano <carloalbertogiordano@hotmail.com>
2025-07-15 09:51:32 +0200

    Fix give same cost to same nodes on multiple calls

--- cost_functions/random_cost.py
+++ cost_functions/random_cost.py
@@ -5,15 +5,23 @@ from math import ceil
 
 
 class RandomCostFunction(CostFunction):
+    cache = {}
+
     def calculate_cost(self, **kwargs):
         """
         Parameters in kwargs:
             - rangeLow (int): default 1
             - rangeMax (int): default 10
         """
-        rangeLow = kwargs.get("rangeLow", 1)
         node_label = kwargs.get("node_label")
+
+        if node_label in self.cache.keys():
+            return self.cache[node_label]
+
+        rangeLow = kwargs.get("rangeLow", 1)
         graph = kwargs.get("graph")
         degree = graph.get_degree(node_label) 
         rangeMax = kwargs.get("rangeMax", ceil(degree/2))
-        return random.randint(rangeLow, rangeMax)
+        result = random.randint(rangeLow, rangeMax)
+        self.cache[node_label] = result
+        return result
